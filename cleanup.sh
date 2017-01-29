b69aa29906c149601fe4b9def7799417016a33ef
BrianLusina <lusinabrian@gmail.com>
2017-01-29 08:38:04 +0300

    Update Configurations
    Add testing, production and development configuration(s)
    These will be used to enable creation of the app under different environments

---
+++ cleanup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export CODACY_PROJECT_TOKEN="376adf775d994c48ae3c914c5b8a245e"
+export SECRET_KEY="a4b814b8)1ecafedbRuU8fX719'ae7fob13"
+export FLASK_COVERAGE="1"
+export CSRF_SESSION_KEY="dfgbodbfa63r3ohnafd0apthn4"
--- config.py
+++ config.py
@@ -2,8 +2,68 @@ import os
 
 basedir = os.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
+    """
+    Global configuration from which other configs inherit
+    :cvar THREADS_PER_PAGE: Application threads. A common general assumption is
+    using 2 per available processor cores - to handle
+    incoming requests using one and performing background
+    operations using the other.
+    :cvar CSRF_SESSION_KEY Use a secure, unique and absolutely secret key for signing the data.
+    """
     SECRET_KEY = os.environ.get()
     CSRF_ENABLE = True
-    CSRF_SESSION_KEY = os.environ.get()
+    CSRF_SESSION_KEY = os.environ.get("CSRF_SESSION_KEY")
     DEBUG = False
+    CSRF_ENABLED = True
+    THREADS_PER_PAGE = 2
+    DATABASE_CONNECT_OPTIONS = {}
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+
+
+class DevelopmentConfig(Config):
+    """
+    Configurations for production environment
+    We set the DEBUG value to True, to enable logging and also run tests
+    """
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    :cvar DEBUG, set to true to allow for running tests and logging
+    :cvar TESTING, set to True to allow for running tests
+    :cvar WTF_CSRF_ENABLED, this is for forms and is set to FALSE, in order to allow for tests
+    :cvar PRESERVE_CONTEXT_ON_EXCEPTION, Enables this app to keep running in the same context even on
+     occurence of an error
+    :cvar SQLALCHEMY_DATABASE_URI, the db uri that will be used for testing, this will not touch the
+      production db, and is faster when it comes to running tests
+    """
+    DEBUG = True
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    WTF_CSRF_ENABLED = False
+    CSRF_ENABLED = False
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
+
+
+class ProductionConfig(Config):
+    """
+    Production configuration that will run when the application is in production
+
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+
+# configuration dictionary that will be imported in other parts of the application and will be used to
+# set up the app under different conditions, testing, development, production..etc
+# the default configuration will be development configuration, this is in case, we forget to set one
+config = {
+    "testing": TestingConfig,
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig
+}
