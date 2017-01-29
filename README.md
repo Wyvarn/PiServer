32ff454418b1c1668fb263137145b95e5ed979e2
BrianLusina <lusinabrian@gmail.com>
2017-01-29 10:32:43 +0300

    remove comments and update readme

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/7dd28ce2d89d414996e3245aff7e819d)](https://www.codacy.com/app/Quilliam/uploader?utm_source=github.com&utm_medium=referral&utm_content=Quilliam/uploader&utm_campaign=badger)
 [![Build Status](https://travis-ci.org/Quilliam/uploader.svg?branch=master)](https://travis-ci.org/Quilliam/uploader)
-[![Coverage Status](https://coveralls.io/repos/github/Quilliam/uploader/badge.svg)](https://coveralls.io/github/Quilliam/uploader)
 [![CircleCI](https://circleci.com/gh/Quilliam/uploader.svg?style=svg)](https://circleci.com/gh/Quilliam/uploader)
 [![codecov](https://codecov.io/gh/Quilliam/uploader/branch/master/graph/badge.svg)](https://codecov.io/gh/Quilliam/uploader)
 [![Codacy Badge](https://api.codacy.com/project/badge/Coverage/7dd28ce2d89d414996e3245aff7e819d)](https://www.codacy.com/app/Quilliam/uploader?utm_source=github.com&utm_medium=referral&utm_content=Quilliam/uploader&utm_campaign=Badge_Coverage)
--- tests/__init__.py
+++ tests/__init__.py
@@ -46,56 +46,5 @@ class BaseTestCase(ContextTestCase):
     def tearDown(self):
         self.app_context.pop()
 
-        # @staticmethod
-        # def create_user_account():
-        #     """
-        #     Create a fictional Author for testing
-        #     :return:
-        #     """
-        #     author = AuthorAccount.query.filter_by(email="guydemaupassant@hadithi.com").first()
-        #     if author is None:
-        #         try:
-        #             author = AuthorAccount(full_name="Guy De Maupassant", email="guydemaupassant@hadithi.com",
-        #                                    password="password", registered_on=datetime.now())
-        #             db.session.add(author)
-        #         except IntegrityError as ie:
-        #             print(ie)
-        #             db.session.rollback()
-        #     return author
-        #
-        # def login(self):
-        #     """
-        #     Login in the user to the testing app
-        #     :return: The authenticated user for the test app
-        #     """
-        #     return self.client.post(
-        #         url_for("auth.login"),
-        #         data=dict(email='guydemaupassant@hadithi.com', password='password', confirm='password'),
-        #         follow_redirects=True
-        #     )
-        #
-        # def add_file(self):
-        #     """
-        #     Adds a dummy story to the database
-        #     :return:
-        #     """
-        #     author = self.create_user_account()
-        #     story = Story.query.filter_by(author_id=author.id).first()
-        #     if story is None:
-        #         try:
-        #             story = Story(title="Gotham in flames", tagline="Dark city catches fire",
-        #                           category="Fiction", content="", author_id=author.id)
-        #             db.session.add(story)
-        #         except IntegrityError as e:
-        #             print(e)
-        #             db.session.rollback()
-        #     return story
-        #
-        # def save_user_file(self, story_id):
-        #     return self.client.get(
-        #         url_for("story.save_story", app_id=story_id),
-        #         follow_redirects=True)
-
-
 if __name__ == "__main__":
     unittest.main()
