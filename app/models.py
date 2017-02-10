0151d2a6c8773d712db761f1b818f2ef4107e8f5
Brian Lusina <lusinabrian@gmail.com>
2017-02-10 20:23:45 +0000

    Add UserAccount table
    This table will contain user account information and will be used to store sensitive data such as username, email, passwords
    Will be used to authenticate user into the system and will hash and verify the hashed passwords

--- app/models.py
+++ app/models.py
@@ -47,10 +47,75 @@ class UserProfile(Base):
     first_name = Column(String(255), nullable=False)
     last_name = Column(String(255), nullable=False)
     full_name = "{} {}".format(first_name, last_name)
-    email = Column(String(500), nullable=False)
+    email = Column(String(500), nullable=False, unique=True)
     accept_terms = Column(Boolean, nullable=False, default=False)
 
     def __repr__(self):
         return "FullName: <FirstName: {}, LastName:{}>\n Email:{}\n".format(self.first_name,
                                                                             self.last_name, self.email)
 
+
+class UserAccount(db.Model):
+    """
+    User account table containing all sensitive account information, like passwords, username, etc
+    :cvar uid, unique user id that will be auto-generated
+    :cvar user_profile_id, the user account id that will be a foreign key and will be linked to
+    UserProfile table
+    :cvar username, the user's user name which will have a default value of email
+    :cvar email, user email that will be linked to the UserProfile
+    :cvar password_hash password that will be hashed and hidden
+    :cvar admin, whether the user is an administrator in the system
+    :cvar confirmed, whether this account has been confirmed
+    :cvar confirmed_on, when this account was confirmed
+    :cvar registered_on, when this account was first registered
+    """
+    __tablename__ = "user_account"
+    uid = Column(String(250), default=str(uuid.uuid4()), nullable=False)
+    user_profile_id = Column(Integer, ForeignKey(UserProfile.id), primary_key=True)
+    username = Column(String(500), default=UserProfile.email, nullable=True, unique=True)
+    email = Column(String(500), default=UserProfile.email, nullable=False, unique=True, onupdate=UserProfile.email)
+    password_hash = Column(String(250), nullable=False)
+    admin = Column(Boolean, nullable=True, default=False)
+    registered_on = Column(DateTime, nullable=False)
+    confirmed = Column(Boolean, nullable=False, default=False)
+    confirmed_on = Column(DateTime, nullable=True)
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        """
+        Sets the password for the user and generates a password hash
+        :param password: user input password
+        """
+        self.password_hash = generate_password_hash(password)
+
+    @password.getter
+    def password(self):
+        """
+        Getter password attribute to fetch the password, This will return the hashed password however
+        At no point will the real password be revealed
+        """
+        return self.password_hash
+
+    def verify_password(self, password):
+        """
+        Verify a user's password to enable authentication into the system
+        This will check the password hash against the input user password,
+        :param password, the password to verify
+        :return:
+        """
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        """
+        Return a formatted and human readable format
+        :return: A human readable UserAccount object
+        """
+        return "ID <uid: {} User Profile Id: {}>\n Username: {}\n Email: {}\n Admin: {}\n Registered on:" \
+               " {}\n Confirmed:< Is confirmed: {}, Date: {}>\n ".format(self.uid, self.user_profile_id,
+                                                                         self.username, self.email,
+                                                                         self.admin, self.registered_on,
+                                                                         self.confirmed, self.confirmed_on)
