3270e691bc0884f3e135b5001181e7b59ef0de7f
BrianLusina <lusinabrian@gmail.com>
2017-02-12 11:58:17 +0300

    add tests for login and register forms

--- app/forms.py
+++ app/forms.py
@@ -17,17 +17,36 @@ class LoginForm(FlaskForm):
     login_button = SubmitField("LOG IN")
     remember_me = BooleanField()
 
-    # def __init__(self, email, password, login_button, remember_me):
-    #     self.email = email
-    #     self.password = password
-    #     self.login_button = login_button
-    #     self.remember_me = remember_me
-
 
 class RegisterForm(FlaskForm):
     """
     Register form for new users
     """
+    first_name = StringField(validators=[DataRequired()])
+    last_name = StringField(validators=[DataRequired()])
+    email = StringField(validators=[DataRequired(), Email()])
+    password = PasswordField(validators=[DataRequired(),
+                                         EqualTo("verify_password", message="Passwords must match"),
+                                         Length(min=8, max=15)])
+    verify_password = PasswordField(validators=[DataRequired()])
+    register_button = SubmitField("REGISTER")
+
+    def validate_form(self):
+        """
+        Validates the form by checking the database for the email submitted
+        This way the register form will communicate directly with the Database before
+        submitting data
+        :return: True or false based on the response from the db
+        :rtype: bool
+        """
+        initial_validation = super(RegisterForm, self).validate()
+        if not initial_validation:
+            return False
+        user = PiCloudUserAccount.query.filter_by(email=self.email.data).first()
+        if user:
+            self.email.errors.append("Email already exists")
+            return False
+        return True
 
 
 class RecoverPasswordForm(FlaskForm):
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -17,5 +17,56 @@ class TestLoginForm(BaseTestCase):
         login_form = LoginForm(email="picloud", password="picloud_mam")
         self.assertFalse(login_form.validate())
 
+
+class TestRegisterForm(BaseTestCase):
+    """
+    Tests for RegisterForm
+
+    """
+
+    def test_validate_successful_register(self):
+        """Test the correct data is validated"""
+        form = RegisterForm(
+            first_name="picloud",
+            last_name="man",
+            email="picloud_man@picloud.com",
+            password="picloudman",
+            verify_password="picloudman"
+        )
+        self.assertTrue(form.validate())
+
+    def test_checks_for_invalid_password_lengths(self):
+        """Checks that invalid password lengths are not allowed"""
+        form = RegisterForm(
+            first_name="picloud",
+            last_name="man",
+            email="picloud_man@picloud.com",
+            password="picl",
+            verify_password="picl"
+        )
+        self.assertFalse(form.validate())
+
+    def test_validate_email_already_registerd(self):
+        """Test that register form does not validate an already registerd email address"""
+        form = RegisterForm(
+            first_name="picloud",
+            last_name="man",
+            email="picloud_man@picloud.com",
+            password="picloud",
+            verify_password="picloud"
+        )
+        self.assertFalse(form.validate_form())
+
+    def test_validate_both_passwords_match(self):
+        """Tests that both passwords should match"""
+        form = RegisterForm(
+            first_name="picloud",
+            last_name="man",
+            email="picloud_man@picloud.com",
+            password="piclould",
+            verify_password="picl"
+        )
+        self.assertFalse(form.validate())
+
 if __name__ == '__main__':
     unittest.main()
