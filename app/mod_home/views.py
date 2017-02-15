b0a39f1a78c61b9e356b3018f545215a8b552512
BrianLusina <lusinabrian@gmail.com>
2017-02-15 19:53:38 +0300

    add home templates

--- app/mod_home/views.py
+++ app/mod_home/views.py
@@ -1,2 +1,31 @@
 from . import home
+from flask import render_template, redirect, flash, request
 
+
+@home.route("")
+@home.route("index")
+@home.route("home")
+def index():
+    """
+    Main view, or home page of the application
+    :return: renders home page template
+    """
+    return render_template("home/index.html")
+
+
+@home.route("contact")
+def contact():
+    """
+    Contact page
+    :return: renders the contact page template
+    """
+    return render_template("home/contact.html")
+
+
+@home.route("about")
+def about():
+    """
+    About page
+    :return: renders template for the about page
+    """
+    return render_template("home/about.html")
