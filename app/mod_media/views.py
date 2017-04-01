d2952fae9a006c2a781a708d5c12aafaae2da209
BrianLusina <lusinabrian@gmail.com>
2017-04-01 15:32:49 +0300

    Update to viewing folders
    the folders are being viewed although not in a clean way. discovered that the getuser() function must be repetitively used which is not DRY principle
    in case one forgets to add this, then it will cause a crash or display an error that should not be there

--- app/mod_media/views.py
+++ app/mod_media/views.py
@@ -18,10 +18,25 @@ def view_media(drive):
     return render_template("media/media.html", drive_name=drive, drive=drive_folders)
 
 
-@media.route("<drive_name>/<folder>")
-def view_folder_in_drive(drive_name, folder):
+@media.route("<drive_name>/<folder_or_file>")
+def view_folder_in_drive(drive_name, folder_or_file):
     """
-    
-    :param drive: 
-    :return: 
+    Enables viewing a particular folder in the drive or the file
+    :param folder_or_file: the folder to open or the file to view
+    :param drive_name: the name of the connected drive
+    :return: view of the folder or the file 
     """
+    root_path = "/media/{}/{}/{}".format(getuser(), drive_name, folder_or_file)
+
+    # perform a check to determine if the folder is a folder or a file
+    if os.path.isdir(root_path):
+        # view the directory
+        folders = os.listdir(root_path)
+        return render_template("media/media.html", drive_name=drive_name, drive=folders)
+
+    elif os.path.isfile(root_path):
+        # view the file
+        file = os.open(root_path, flags=0)
+        return render_template("media/media.html", drive_name=drive_name, drive=file)
+
+    return render_template("media/media.html", drive_name=drive_name, drive="")
