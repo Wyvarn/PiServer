8811616a13cda0b6c080cfa216438554698c4541
BrianLusina <lusinabrian@gmail.com>
2017-04-01 14:35:06 +0300

    update media template
    media template will be used to display folders and directories in the selected drive
    if the drive has no directories and or folders or files, then a message is displayed, else all the directories or files are displayed

--- app/mod_media/views.py
+++ app/mod_media/views.py
@@ -3,3 +3,15 @@ This will handle media files that are in the media directory if the Pi
 """
 from . import media
 from flask import render_template, redirect, url_for
+import os
+
+
+@media.route("<drive>")
+def view_media(drive):
+    """
+    View media files that are in the drive folder
+    :param drive: 
+    :return: view template for files in the media file
+    """
+    drive_folders = os.listdir("/media/" + drive)
+    return render_template("media/media.html", drive_name=drive, folders=drive_folders)
