a6d21b73993c33252e20955d279627610f99ac9b
Brian Lusina <lusinabrian@gmail.com>
2017-07-03 08:26:39 +0300

    Update Readme with install instructions, create HomePage and update _reset.scss
    Readme now has install instructions as well as docker integration steps
    HomePage container component created and linked to root Component App.js
    _reset.scss modified to _normalize.scss for the base styles to normalize the root styles

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from '../images/logo.svg';
 import {Router, Route, withRouter} from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
+import Home from './home/HomePage';
 import { connect } from 'react-redux';
 
 const history = createHistory();
@@ -11,13 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
           <Router history={history}>
               <div>
-
+                  <Route path="home" component={Home}/>
               </div>
           </Router>
       </div>
